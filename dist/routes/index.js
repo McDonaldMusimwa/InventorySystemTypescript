5351cb89e05eefac1230686264ce1e4e7ff62222
McDonald <mus22003@byui.edu>
2023-11-29 09:03:02 +0200

    ran build
    deleted the dist folder and ran build command

--- dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-router.use('/user', require('./user'));
-router.use('/stock', require('./stock'));
-router.use('/order', require('./order'));
-router.use('/supplier', require('./supplier'));
-module.exports = router;
-//# sourceMappingURL=index.js.map
--- dist/src/app.js
+++ dist/src/app.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,7 +33,7 @@ const PORT = process.env.PORT;
 const mongose = require('mongoose');
 const body_parser_1 = __importDefault(require("body-parser"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const swaggerDocument = require('../swagger-output.json');
+const swaggerDocument = __importStar(require("../swagger-output.json"));
 const passport_1 = __importDefault(require("passport"));
 const express_session_1 = __importDefault(require("express-session"));
 const cors_1 = __importDefault(require("cors"));
